f9d42ea87dee4f01bda94f23607f40ecce76ca42
KimoJi71 <t107ab0048@ntut.org.tw>
2021-10-18 16:32:06 +0800

    add: login view

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,15 @@
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,48 +1,73 @@
 <template>
-  <v-container class="fill-height" fluid>
-    <v-card color="orange lighten-1" width="400" height="700">
-      <v-row align="center" justify="center">
-        <v-col class="mt-15" cols="auto">
-          <v-img src="../../public/專題LOGO-v.png" width="250" height="250" />
-        </v-col>
-      </v-row>
-    </v-card>
-    <v-card color="orange lighten-5" height="700">
-      <v-row class="ma-8">
-        <v-card>
-          <v-card-text>
-            <v-row class="mt-8" align="center" justify="center" no-gutters>
-              <v-col cols="12">
-                <p class="text-h6 text-center">感謝您使用我們的網站</p>
-              </v-col>
-              <v-col cols="12">
-                <p class="text-h6 text-center">登入使用更多功能</p>
-              </v-col>
-            </v-row>
-            <v-row class="mt-6" align="center" justify="center" no-gutters>
-              <v-form>
-                <v-col>
-                  <v-text-field
-                    prepend-inner-icon="mdi-account"
-                    label="帳號"
-                    filled
-                    rounded
-                  />
-                </v-col>
-                <v-col>
-                  <v-text-field
-                    prepend-inner-icon="mdi-lock"
-                    type="password"
-                    label="密碼"
-                    filled
-                    rounded
-                  />
-                </v-col>
-              </v-form>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-row>
-    </v-card>
-  </v-container>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-8">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="5" class="orange lighten-1">
+                      <v-card-text>
+                        <v-img src="../../public/專題LOGO-v.png" class="mt-10" />
+                      </v-card-text>
+                      <div class="text-center mt-4">
+                        <v-btn rounded outlined dark x-large @click="step=2">註冊</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="7">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center grey--text">感謝您使用我們的網站</h1>
+                        <br /><br />
+                        <h1 class="text-center grey--text">登入使用更多功能</h1>
+                        <br />
+                        <v-form class="mt-6">
+                          <v-text-field
+                            label="帳號"
+                            prepend-inner-icon="mdi-account"
+                            color="orange"
+                            filled
+                            rounded
+                          />
+                          <v-text-field
+                            label="密碼"
+                            prepend-inner-icon="mdi-lock"
+                            type="password"
+                            color="orange"
+                            filled
+                            rounded
+                          />
+                        </v-form>
+                        <v-btn color="orange lighten-1" text>忘記密碼？</v-btn>
+                      </v-card-text>
+                      <div class="text-center mt-3 mb-6">
+                        <v-btn color="orange lighten-1" dark rounded x-large>登入</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item value="2">
+
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data() {
+    return {
+      step: 1,
+    };
+  },
+};
+</script>

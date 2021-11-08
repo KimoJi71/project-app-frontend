44e08e30b07a8a3aa1d4f9ca684d94306ee66353
KimoJi71 <t107ab0048@ntut.org.tw>
2021-11-08 12:01:03 +0800

    add: product views

---
+++ src/pages/product/Product.vue
@@ -0,0 +1,155 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div id="bkg">
+    <Header />
+    <br /><br /><br />
+    <v-row class="mt-4" justify="center">
+      <v-col cols="12" md="2">
+        <v-btn icon large v-if="screenWidth < 960" @click="show = !show">
+          <v-icon large>mdi-dots-grid</v-icon>
+        </v-btn>
+        <v-card
+          color="#FFE0B2"
+          tile
+          elevation="0"
+          v-if="screenWidth > 960 || show"
+        >
+          <v-card-text>
+            <v-select
+              label="保險公司"
+              clearable
+              chips
+              dense
+              solo
+              :items="[1, 2, 3]"
+            />
+            <v-select
+              label="保險屬性"
+              clearable
+              chips
+              dense
+              solo
+              :items="[1, 2, 3]"
+            />
+            <v-select
+              label="主附約"
+              clearable
+              chips
+              dense
+              solo
+              :items="[1, 2, 3]"
+            />
+            <v-select
+              label="保險種類"
+              clearable
+              chips
+              dense
+              solo
+              :items="[1, 2, 3]"
+            />
+            <v-select
+              label="保險特色"
+              clearable
+              chips
+              dense
+              solo
+              :items="[1, 2, 3]"
+            />
+            <v-select
+              label="保障內容"
+              clearable
+              chips
+              dense
+              solo
+              :items="[1, 2, 3]"
+            />
+          </v-card-text>
+          <div class="text-center">
+            <v-btn class="mb-6" color="primary" dark rounded large>篩選</v-btn>
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" md="9">
+        <v-row justify="center">
+          <v-col cols="12" sm="4" md="6">
+            <v-btn color="primary lighten-1" tile block depressed>最新</v-btn>
+          </v-col>
+          <v-col cols="12" sm="4" md="6">
+            <v-btn color="primary lighten-1" tile outlined block depressed
+              >最多用戶收藏</v-btn
+            >
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12" md="12">
+            <v-card elevation="3">
+              <v-card-title>
+                南山人壽 ｜ 新活力康祥定期健康保險(NTDD)
+              </v-card-title>
+              <v-card-text>
+                <v-chip color="red" label outlined>
+                  壽險保障商品、醫療保障商品
+                </v-chip>
+                <v-chip class="ml-3" color="success" label outlined>
+                  主約
+                </v-chip>
+                <v-chip class="ml-3" color="blue" label outlined> 定期 </v-chip>
+                <v-btn
+                  text
+                  :style="
+                    screenWidth < 960 ? 'margin-top: 20px' : 'float: right'
+                  "
+                  @click="getDetail()"
+                >
+                  <span class="primary--text">
+                    了解更多
+                    <v-icon color="primary">mdi-arrow-right</v-icon>
+                  </span>
+                </v-btn>
+              </v-card-text>
+
+              <v-divider class="mt-2 ml-4" />
+
+              <div class="mt-4 ml-4">
+                <v-icon class="mb-4" color="red">mdi-heart-outline</v-icon>
+                <v-icon class="ml-2 mb-4" color="success">mdi-share</v-icon>
+                <v-icon class="ml-2 mb-4" color="blue"
+                  >mdi-bookmark-outline</v-icon
+                >
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      screenWidth: document.body.clientWidth,
+      show: false,
+    };
+  },
+  methods: {
+    getDetail() {
+      this.$router.push({ name: "ProductDetail", params: { id: 1 } });
+    },
+  },
+};
+</script>
+
+<style>
+#bkg {
+  background-color: #fdf7ef;
+  height: 100%;
+}
+</style>
---
+++ src/pages/product/ProductDetail.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id="bkg">
+    <Header />
+    <br /><br /><br />
+    <v-card class="mx-auto my-6" max-width="70%" elevation="3">
+      <v-card-title>南山人壽 ｜ 新活力康祥定期健康保險(NTDD)</v-card-title>
+      <v-card-text>
+        <v-chip color="red" label outlined> 壽險保障商品、醫療保障商品 </v-chip>
+        <v-chip class="ml-3" color="success" label outlined> 主約 </v-chip>
+        <v-chip class="ml-3" color="blue" label outlined> 定期 </v-chip>
+
+        <v-list>
+          <v-list-item class="pa-0 mt-4">
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                <v-icon class="mr-2" color="black">mdi-label</v-icon>
+                商品種類
+              </v-list-item-title>
+              <span class="mt-3 pl-9"
+                >定期壽險-台幣 / 重大疾病與重大傷病 / 癌症醫療</span
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item class="pa-0">
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                <v-icon class="mr-2" color="black">mdi-label</v-icon>
+                商品特色
+              </v-list-item-title>
+              <div class="mt-3 pl-9">
+                <ul>
+                  <li>投保年齡：15足歲~40歲</li>
+                  <li class="mt-3">7項重大疾病，輕/重度均有保障</li>
+                  <li class="mt-3">加強人生黃金期的階段性保障</li>
+                </ul>
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item class="pa-0">
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                <v-icon class="mr-2" color="black">mdi-label</v-icon>
+                保障內容
+              </v-list-item-title>
+              <span class="mt-3 pl-9">
+                身故給付 / 完全失能給付 / 重大疾病 / 豁免保費
+              </span>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item class="pa-0">
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                <v-icon class="mr-2" color="black">mdi-label</v-icon>
+                附加檔案
+              </v-list-item-title>
+              <div class="mt-3 ml-9">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      color="blue"
+                      fab
+                      outlined
+                      v-bind="attrs"
+                      v-on="on"
+                      href="https://www.nanshanlife.com.tw/NanshanWeb/file/DOCUMENT/7341"
+                      target="_blank"
+                    >
+                      <v-icon>mdi-clipboard-minus</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>DM</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      class="ml-3"
+                      color="green"
+                      fab
+                      outlined
+                      v-bind="attrs"
+                      v-on="on"
+                      href="https://www.nanshanlife.com.tw/NanshanWeb/file/DOCUMENT/7361"
+                      target="_blank"
+                    >
+                      <v-icon>mdi-file-document</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>條款</span>
+                </v-tooltip>
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+
+      <v-divider class="ml-4" />
+
+      <div class="mt-4 ml-4">
+        <v-icon class="mb-4" color="red">mdi-heart-outline</v-icon>
+        <v-icon class="ml-2 mb-4" color="success">mdi-share</v-icon>
+        <v-icon class="ml-2 mb-4" color="blue">mdi-bookmark-outline</v-icon>
+      </div>
+    </v-card>
+
+    <v-tooltip top>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary lighten-1"
+          dark
+          fixed
+          right
+          bottom
+          fab
+          v-bind="attrs"
+          v-on="on"
+          @click="goBack()"
+        >
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+      </template>
+      <span>回上一頁</span>
+    </v-tooltip>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    goBack() {
+      this.$router.back(-1);
+    },
+  },
+};
+</script>
+
+<style>
+#bkg {
+  background-color: #fdf7ef;
+  height: 100%;
+}
+</style>
